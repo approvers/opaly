7408eba8bab999ae4a937293dc547551b2f788b1
loxygenk <me@loxygen.dev>
2021-08-30 19:28:39 +0900

    🏷️ Add more functions at OppaiRepository

--- src/repository/types.rs
+++ src/repository/types.rs
@@ -1,6 +1,10 @@
+use chrono::Duration;
+
 use crate::model::Oppai;
 
 pub trait OppaiRepository {
     fn add(&mut self, oppai: &Oppai);
-    fn get(&self);
+    fn get(&self) -> Vec<Oppai>;
+    fn count(&self) -> u64;
+    fn get_latest(&self, duration: Duration);
 }
